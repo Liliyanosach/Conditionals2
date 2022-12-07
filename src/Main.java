ba309d8a3ca92fde1df2c111020595a091c7a399
Nosach Liliya <mooduck@icloud.com>
2022-12-07 16:26:56 +0600

    hw conditionals

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,95 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        int clientOS = 1;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //Для iOS — «Установите версию приложения для iOS по ссылке».
+        //Для Android — «Установите версию приложения для Android по ссылке»
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+
+
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        int year = 2000;
+
+        if (year % 4 == 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else if (year % 100 != 0) {
+            System.out.println(year + " год не является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //« …. год является високосным» или «... год не является високосным»
+
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+        int monthsNumber = 7;
+
+        switch (monthsNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+            default:
+                System.out.println("Такого месяца не сущуствует");
+
+        }
     }
 }
